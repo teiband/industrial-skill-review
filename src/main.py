c173ba9b8035d00b3d2f9ed31e5e5271d5d505b6
Thomas Eiband <thomas.eiband@dlr.de>
2021-10-07 13:28:41 +0200

    select only relevant entries, generate wordcloud for multiple columns

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3.6
 import csv
 import pickle
-import matplotlib.pyplot as plt
+import time
+
+from wordcloud import WordCloud
 
 from common import *
 
-results_file = "../data/in/Skill-Taxonomy-Review.csv"
+results_file = "../data/in/Skill-Taxonomy-Review - Review-List.csv"
 
 with open(results_file, 'r') as f:
     lines = f.readlines()
@@ -24,32 +26,52 @@ results = np.array(results)
 header = results[0:2, :]
 results = results[2:, :]
 
-skill_names_raw = results[:, 11]
+selection_symbol = 'Y'
+selection_column = 2
+selected_results = results[results[:, selection_column] == selection_symbol, :]
 
-skill_names_split = [s.split(',') for s in skill_names_raw if s not in ['-', '']] # split by comma
-skill_names_flat = [item for sublist in skill_names_split for item in sublist] # flatten list of list
-skill_names = [camel_case_split(s) for s in skill_names_flat] # resolve camel case into spaces
-skill_names = [n.strip().replace(' ', '_') for n in skill_names] # replace spaces with underscores
-skill_names = [s.lower() for s in skill_names] # make all lower case
 
-skill_names_joined = ", ".join(skill_names) # join to single string
+def preprocess_spelling(input_list, split_by_comma=True, camel_case_to_spaces=True, spaces_to_underscores=True,
+                        to_lowercase=True):
+    if split_by_comma:
+        output_list = [s.split(',') for s in input_list if s not in ['-', '']]  # split by comma
+        output_list = [item for sublist in output_list for item in sublist]  # flatten list of list
+    if camel_case_to_spaces:
+        output_list = [camel_case_split(s) for s in output_list]  # resolve camel case into spaces
+    if spaces_to_underscores:
+        output_list = [n.strip().replace(' ', '_') for n in output_list]  # replace spaces with underscores
+    if to_lowercase:
+        output_list = [s.lower() for s in output_list]  # make all lower case
+    return output_list
 
-# Plot word cloud
-# Define a function to plot word cloud
+
 def plot_cloud(wordcloud):
-    # Set figure size
     plt.figure(figsize=(40, 30))
-    # Display image
     plt.imshow(wordcloud)
-    # No axis details
     plt.axis("off");
 
-# Import package
-from wordcloud import WordCloud, STOPWORDS
-# Generate word cloud
-wordcloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='salmon', colormap='Pastel1', collocations=False, stopwords=None).generate(skill_names_joined)
-# Plot
-plot_cloud(wordcloud)
+
+column_mapping = {'Skill': 10,
+                  'IdentifiedSkills': 11,
+                  'Primitives': 12,
+                  'IdentifiedPrimitives': 13}
+
+start = time.time()
+for key, value in column_mapping.items():
+    label = key
+    input_list = selected_results[:, value]
+    output_list = preprocess_spelling(input_list=input_list)
+    output_joined = ", ".join(output_list)  # join to single string
+
+    # Generate word cloud
+    wordcloud = WordCloud(width=3000, height=2000, random_state=1, background_color='salmon', colormap='Pastel1',
+                          collocations=False, stopwords=None).generate(output_joined)
+    # Plot
+    plot_cloud(wordcloud)
+    plt.title(key)
+
+now = time.time()
+print(f"duration:{now - start}")
 plt.show()
 
 exit()
@@ -70,7 +92,8 @@ all_skill_names = split_names_into_words(lemma_skill_names)
 plot_file_path = "../../../papers/2021-interactive-contact-class-paper/figures/user_study_round_1.pdf"
 plot_file_path_single_figs = "../../../papers/2021-interactive-contact-class-paper/figures/user_study_round_1_"
 
-ordered_skill_names, frequency_skill_names, fig, single_figs = process_skill_names(subplot_titles, all_skill_names, rows=2, cols=4)
+ordered_skill_names, frequency_skill_names, fig, single_figs = process_skill_names(subplot_titles, all_skill_names,
+                                                                                   rows=2, cols=4)
 
 with open('../data/skill_names.txt', 'w') as f:
     f.writelines(ordered_skill_names.__str__())
@@ -79,7 +102,6 @@ with open('../data/common_skill_names.txt', 'w') as f:
 
 data = (ordered_skill_names, frequency_skill_names)
 pickle.dump(data, open("../data/data.pickle", 'wb'))
-
 
 fig.update_layout(showlegend=False,
                   autosize=False,
@@ -94,11 +116,11 @@ fig.update_layout(showlegend=False,
                   font_size=13,
                   font_family="Times New Roman",
                   )
-#fig.write_image(plot_file_path)
+# fig.write_image(plot_file_path)
 fig.show()
 
 for f, t in zip(single_figs, subplot_titles):
-    #f.savefig(plot_file_path_single_figs + t + '.pdf')
+    # f.savefig(plot_file_path_single_figs + t + '.pdf')
     f.show()
 
 plt.show()
